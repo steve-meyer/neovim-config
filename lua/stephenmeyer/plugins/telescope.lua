fdf64a946182229e57a665b2790bea5ad8c92539
Steve Meyer <steve.e.meyer@gmail.com>
2026-03-13 08:17:48 -0500

    Experimenting with keymaps for telescope

--- lua/stephenmeyer/plugins/telescope.lua
+++ lua/stephenmeyer/plugins/telescope.lua
@@ -8,8 +8,8 @@ return {
     },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>ff", builtin.git_files,  { desc = "Telescope find git files" })
+      vim.keymap.set("n", "<leader>fa", builtin.find_files, { desc = "Telescope find all files" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope find buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope help tags" })
     end
